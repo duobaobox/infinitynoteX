d13c3dd59a59d81bcc745aa480f8c287b86ee080
duobao0007 <duobao0007@noreply.gitcode.com>
2025-10-26 01:34:27 +0800

    feat(app): 更新布局样式，优化根容器和面板样式

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="container-debug">应用拖拽区域</div>;
+  return (
+    <>
+      <div className="layout-panel drag-area">应用拖拽区域</div>
+      <div className="layout-panel main-content">主区域</div>
+    </>
+  );
 }
 
 export default App;

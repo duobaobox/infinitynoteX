8a3d94f53a3b31839257c32dae90b7f4d6a48035
duobao0007 <duobao0007@noreply.gitcode.com>
2025-10-26 16:47:30 +0800

    feat(AIWorkbench): 添加AI工作台组件，集成@ant-design/x的Sender功能

---
+++ src/components/AIWorkbench/AIWorkbench.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Sender } from "@ant-design/x";
+
+const AIWorkbench: React.FC = () => {
+  return (
+    <div>
+      <Sender />
+    </div>
+  );
+};
+
+export default AIWorkbench;

f5bc8cc8bfdbcc8e2298cd19c1b18589050378b2
duobao0007 <duobao0007@noreply.gitcode.com>
2025-10-26 16:21:15 +0800

    feat(EditorPanel): 添加分段控件以选择编辑模式，优化编辑面板功能

--- src/components/EditorPanel.tsx
+++ src/components/EditorPanel.tsx
@@ -1,7 +1,58 @@
 import React from "react";
 
+import { Segmented } from "antd";
+import {
+  EditOutlined,
+  ToolOutlined,
+  RobotOutlined,
+  AppstoreOutlined,
+} from "@ant-design/icons";
+
 const EditorPanel: React.FC = () => {
-  return <div className="layout-panel editor-container"></div>;
+  const segmentOptions = [
+    {
+      label: (
+        <span>
+          <EditOutlined style={{ marginRight: 4 }} />
+        </span>
+      ),
+      value: "edit",
+    },
+    {
+      label: (
+        <span>
+          <ToolOutlined style={{ marginRight: 4 }} />
+        </span>
+      ),
+      value: "tools",
+    },
+    {
+      label: (
+        <span>
+          <RobotOutlined style={{ marginRight: 4 }} />
+        </span>
+      ),
+      value: "ai",
+    },
+    {
+      label: (
+        <span>
+          <AppstoreOutlined style={{ marginRight: 4 }} />
+        </span>
+      ),
+      value: "other",
+    },
+  ];
+  return (
+    <div className="layout-panel editor-container">
+      <div className="flex-vertical-equal">
+        <div style={{ display: "inline-block", marginBottom: 16 }}>
+          <Segmented options={segmentOptions} defaultValue="edit" />
+        </div>
+        {/* 编辑内容放这里 */}
+      </div>
+    </div>
+  );
 };
 
 export default EditorPanel;

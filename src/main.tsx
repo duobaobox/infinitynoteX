28b18d481a8e3452533e475df81ae1245173ef15
duobao0007 <duobao0007@noreply.gitcode.com>
2025-10-26 13:00:39 +0800

    feat(app): 添加AntdConfig组件以统一Ant Design全局配置

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import AntdConfig from "./theme/AntdConfig";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <AntdConfig>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </AntdConfig>
 );
 
 // Use contextBridge
---
+++ src/theme/AntdConfig.tsx
@@ -0,0 +1,25 @@
+import { ConfigProvider, theme } from "antd";
+import zhCN from "antd/locale/zh_CN";
+import React from "react";
+
+/**
+ * Ant Design 全局配置组件
+ * 用于统一主题、国际化、全局Token等配置
+ */
+const AntdConfig: React.FC<{ children: React.ReactNode }> = ({ children }) => (
+  <ConfigProvider
+    locale={zhCN}
+    theme={{
+      token: {
+        colorPrimary: "#fa8c16", // 主色
+        borderRadius: 6, // 全局圆角
+        // 可继续添加其他全局Token
+      },
+      algorithm: theme.defaultAlgorithm,
+    }}
+  >
+    {children}
+  </ConfigProvider>
+);
+
+export default AntdConfig;
